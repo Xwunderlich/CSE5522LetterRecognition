de6c568b7c5b1caff3bcf776c847bd138fd6eb91
Chuhan Feng <simonrouse9461@gmail.com>
2016-12-06 13:31:03 -0500

    update

--- MNISTFeatureExtractor.py
+++ MNISTFeatureExtractor.py
@@ -1,10 +1,6 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 
-# helper method to get a pixel value by row and column
-def pixel(img, row, col):
-	return img[28 * row + col]
-
 class MNISTFeatureExtractor:
 				
 	def __init__(self, directory='MNIST_data/'):
@@ -21,118 +17,25 @@ class MNISTFeatureExtractor:
 	def load(self):
 		self.mnist = input_data.read_data_sets(self.directory, one_hot=False)
 	
-	# extract the statistical features
-	def statistical_feature(self):
-		
-		def isOn(img, row, col):
-			return pixel(img, row, col) >= 0.5
-
-		def offLeft(img, row, col):
-			return col == 0 or not isOn(img, row, col - 1)
-			
-		def offAbove(img, row, col):
-			return row == 0 or not isOn(img, row - 1, col)
-	
-		# extract 16 features
-		def extract(image):
-			count, countVE, countHE = 0, 0, 0
-			minRow, minCol = None, None
-			maxRow, maxCol = None, None
-			sumX, sumY = 0, 0
-			sumX2, sumY2, sumXY = 0, 0, 0
-			sumX2Y, sumY2X = 0, 0
-			sumXHE, sumYVE = 0, 0
-			for i, pixel in enumerate(image):
-				row, col = i // 28, i % 28
-				x, y = col - 14, row - 14
-				if isOn(image, row, col):
-					count += 1
-					sumX += x
-					sumY += y
-					sumX2 += x**2
-					sumY2 += y**2
-					sumXY += x*y
-					sumX2Y += x**2*y
-					sumY2X += y**2*x
-					if offLeft(image, row, col):
-						countVE += 1
-						sumYVE += y
-					if offAbove(image, row, col):
-						countHE += 1
-						sumXHE += x
-					if minRow is None or minRow > row:
-						minRow = row
-					if minCol is None or minCol > col:
-						minCol = col
-					if maxRow is None or maxRow < row:
-						maxRow = row
-					if maxCol is None or maxCol < col:
-						maxCol = col
-			_1 = minCol
-			_2 = minRow
-			_3 = maxCol - minCol
-			_4 = maxRow - minRow
-			_5 = count
-			_6 = sumX / count
-			_7 = sumY / count
-			_8 = sumX2 / count
-			_9 = sumY2 / count
-			_10 = sumXY / count
-			_11 = sumX2Y / count
-			_12 = sumY2X / count
-			_13 = countVE
-			_14 = sumYVE
-			_15 = countHE
-			_16 = sumXHE
-			return _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16
-		
+	def extract(self, method):
 		# for each image in training set, extract features and push it into self.features
 		# also push the label into self.labels
 		for tr_image, tr_label in zip(self.mnist.train.images, self.mnist.train.labels):
-			self.features.append(list(extract(tr_image)))
+			self.features.append(list(method(tr_image)))
 			self.labels.append(tr_label)
 			print(len(self))
 		# for each image in testing set, extract features and push it into self.features
 		# also push the label into self.labels
 		for te_image, te_label in zip(self.mnist.test.images, self.mnist.test.labels):
-			self.features.append(list(extract(te_image)))
+			self.features.append(list(method(te_image)))
 			self.labels.append(te_label)
 			print(len(self))
-			
-	# TODO
-	def diagonal_extraction(self):
-		def zone_pixel_on(img, zone, row, col):
-			zone_row = zone // 4 * 7 + row
-			zone_col = zone % 4 * 7 + col
-			return pixel(img, zone_row, zone_col) >= 0.5
-			
-		def evaluate_zone(img, zone):
-			diagonals = [0] * 13
-			for row in range(7):
-				for col in range(7):
-					diagonals[row + col] += zone_pixel_on(img, zone, row, col)
-			return np.ndarray(diagonals).mean
-			
-		def extract(image):
-			for zone in range(16):
-				pass
-			
-		for tr_image, tr_label in zip(self.mnist.train.images, self.mnist.train.labels):
-			self.features.append(list(extract(tr_image)))
-			self.labels.append(tr_label)
-			print(len(self))
-		# for each image in testing set, extract features and push it into self.features
-		# also push the label into self.labels
-		for te_image, te_label in zip(self.mnist.test.images, self.mnist.test.labels):
-			self.features.append(list(extract(te_image)))
-			self.labels.append(te_label)
-			print(len(self))
-
-			
-	# normalize the data to make features range from -1024 to 1024
-	def normalize(self):
+		# convert to numpy array
 		self.features = np.array(self.features)
 		self.labels = np.array(self.labels)
+						
+	# normalize the data to make features range from -1024 to 1024
+	def normalize(self):
 		abs_max_attr = np.amax(np.abs(self.features), axis=0)
 		self.normalize_factor = 1024 / abs_max_attr
 		for i in range(len(self.features)):
@@ -149,10 +52,95 @@ class MNISTFeatureExtractor:
 				out.write('\n')
 		
 
-extractor = MNISTFeatureExtractor()
-extractor.load()
-extractor.statistical_feature()
-extractor.normalize()
-extractor.output('MNIST.data')
+# helper method to get a pixel value by row and column
+def pixel_value(img, row, col):
+	return img[28 * row + col]
+
+def statistical_features(image):
+	def isOn(img, row, col):
+		return pixel_value(img, row, col) >= 0.5
+
+	def offLeft(img, row, col):
+		return col == 0 or not isOn(img, row, col - 1)
+		
+	def offAbove(img, row, col):
+		return row == 0 or not isOn(img, row - 1, col)
+		
+	count, countVE, countHE = 0, 0, 0
+	minRow, minCol = None, None
+	maxRow, maxCol = None, None
+	sumX, sumY = 0, 0
+	sumX2, sumY2, sumXY = 0, 0, 0
+	sumX2Y, sumY2X = 0, 0
+	sumXHE, sumYVE = 0, 0
+	for i, pixel in enumerate(image):
+		row, col = i // 28, i % 28
+		x, y = col - 14, row - 14
+		if isOn(image, row, col):
+			count += 1
+			sumX += x
+			sumY += y
+			sumX2 += x**2
+			sumY2 += y**2
+			sumXY += x*y
+			sumX2Y += x**2*y
+			sumY2X += y**2*x
+			if offLeft(image, row, col):
+				countVE += 1
+				sumYVE += y
+			if offAbove(image, row, col):
+				countHE += 1
+				sumXHE += x
+			if minRow is None or minRow > row:
+				minRow = row
+			if minCol is None or minCol > col:
+				minCol = col
+			if maxRow is None or maxRow < row:
+				maxRow = row
+			if maxCol is None or maxCol < col:
+				maxCol = col
+	_1 = minCol
+	_2 = minRow
+	_3 = maxCol - minCol
+	_4 = maxRow - minRow
+	_5 = count
+	_6 = sumX / count
+	_7 = sumY / count
+	_8 = sumX2 / count
+	_9 = sumY2 / count
+	_10 = sumXY / count
+	_11 = sumX2Y / count
+	_12 = sumY2X / count
+	_13 = countVE
+	_14 = sumYVE
+	_15 = countHE
+	_16 = sumXHE
+	return _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16
+
+# diagonal base feature extraction
+def diagonal_features(image):
+	def zone_pixel_on(img, zone, row, col):
+		zone_row = zone // 4 * 7 + row
+		zone_col = zone % 4 * 7 + col
+		return pixel_value(img, zone_row, zone_col) >= 0.5
+		
+	def evaluate_zone(img, zone):
+		diagonals = [0] * 13
+		for row in range(7):
+			for col in range(7):
+				diagonals[row + col] += zone_pixel_on(img, zone, row, col)
+		return np.ndarray(diagonals).mean
+		
+	features = [0] * 16
+	for zone in range(16):
+		features[zone] = evaluate_zone(image, zone)
+	return features
+		
+		
+extractor = MNISTFeatureExtractor()	
+extractor.load()							# load mnist
+extractor.extract(diagonal_features)		# pass your feature extractor as the parameter
+extractor.normalize()						# normalize dataset
+extractor.output('MNIST.data')				# write to file
 			
 			
