12a0ae3b8d14563abdfe3a778cc5f2ea5be49c07
Chuhan Feng <simonrouse9461@gmail.com>
2016-12-07 14:00:35 -0500

    update

--- testing.py
+++ testing.py
@@ -13,11 +13,11 @@ dataset = DatasetLoader('Stanford_data/raw.data', class_list=DatasetLoader.lower
 dataset.load(one_hot=True)
 dataset.fold(10, 0)
 
-neural_net = NNBuilder(dataset.attr_count, dataset.class_count)
-neural_net.add_layer(400, activation=tf.nn.relu)
-neural_net.add_layer(400, activation=tf.nn.relu)
-neural_net.add_layer(400, activation=tf.nn.relu)
-neural_net.finish()
+neural_net = NNBuilder(dataset.attr_count, dataset.class_count)		# create a neural net with the number of input and the number of output
+neural_net.add_layer(400, activation=tf.nn.relu)						# add a ReLU layer with certain amount of nodes
+neural_net.add_layer(400, activation=tf.nn.relu)						# add a ReLU layer with certain amount of nodes
+neural_net.add_layer(400, activation=tf.nn.relu)						# add a ReLU layer with certain amount of nodes
+neural_net.finish()														# finish building the neural net
 neural_net.train(dataset, rate=0.0000005, iteration=30000, peek_interval=100)
 accuracy, loss = neural_net.evaluate(dataset)
 time = format_time(neural_net.time)
