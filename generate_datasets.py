c589b66e054faccbd5ae214f53213a4ae05b8e1f
Chuhan Feng <simonrouse9461@gmail.com>
2016-12-07 18:04:50 -0500

    update

--- generate_datasets.py
+++ generate_datasets.py
@@ -23,6 +23,12 @@ def generate_MNIST_diagonal(path='MNIST_data/diagonal.data', test=None):
 	generator.load_MNIST()
 	generator.extract(fd.diagonal_features, parameters={'zone_size': 4}, normalize_to=256, test=test)	
 	generator.output(path, int_feature=True)	
+	
+def generate_MNIST_statistical(path='MNIST_data/statistical.data', test=None):
+	generator = DatasetGenerator()
+	generator.load_MNIST()
+	generator.extract(fd.statistical_features, normalize_to=256, test=test)	
+	generator.output(path, int_feature=False)
 
 def generate_MNIST_raw(path='MNIST_data/raw.data', test=None):
 	generator = DatasetGenerator()
