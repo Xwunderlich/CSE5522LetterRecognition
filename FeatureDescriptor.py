9b46dc48ba78f079f8c7225b7b8993e20f680a0e
xwunderlich <xanderwunderlich@gmail.com>
2016-12-08 23:34:30 -0500

    hot spot feature extraction and added .data files to git ignore

--- FeatureDescriptor.py
+++ FeatureDescriptor.py
@@ -2,81 +2,124 @@ import statistics as stat
 import numpy as np
 
 def statistical_features(image):
-	count, countVE, countHE = 0, 0, 0
-	minRow, minCol = None, None
-	maxRow, maxCol = None, None
-	sumX, sumY = 0, 0
-	sumX2, sumY2, sumXY = 0, 0, 0
-	sumX2Y, sumY2X = 0, 0
-	sumXHE, sumYVE = 0, 0
-	for pixel, (row, col) in image.on_pixels():
-		x = col - image.width / 2
-		y = row - image.height / 2
-		count += 1
-		sumX += x
-		sumY += y
-		sumX2 += x**2
-		sumY2 += y**2
-		sumXY += x*y
-		sumX2Y += x**2*y
-		sumY2X += y**2*x
-		if image.is_off(row, col, offset=(0, -1)):
-			countVE += 1
-			sumYVE += y
-		if image.is_off(row, col, offset=(-1, 0)):
-			countHE += 1
-			sumXHE += x
-		if minRow is None or minRow > row:
-			minRow = row
-		if minCol is None or minCol > col:
-			minCol = col
-		if maxRow is None or maxRow < row:
-			maxRow = row
-		if maxCol is None or maxCol < col:
-			maxCol = col
-	features = [0] * 16
-	features[0] = minCol
-	features[1] = minRow
-	features[2] = maxCol - minCol
-	features[3] = maxRow - minRow
-	features[4] = count
-	features[5] = sumX / count
-	features[6] = sumY / count
-	features[7] = sumX2 / count
-	features[8] = sumY2 / count
-	features[9] = sumXY / count
-	features[10] = sumX2Y / count
-	features[11] = sumY2X / count
-	features[12] = countVE
-	features[13] = sumYVE
-	features[14] = countHE
-	features[15] = sumXHE
-	return features
+  count, countVE, countHE = 0, 0, 0
+  minRow, minCol = None, None
+  maxRow, maxCol = None, None
+  sumX, sumY = 0, 0
+  sumX2, sumY2, sumXY = 0, 0, 0
+  sumX2Y, sumY2X = 0, 0
+  sumXHE, sumYVE = 0, 0
+  for pixel, (row, col) in image.on_pixels():
+    x = col - image.width / 2
+    y = row - image.height / 2
+    count += 1
+    sumX += x
+    sumY += y
+    sumX2 += x**2
+    sumY2 += y**2
+    sumXY += x*y
+    sumX2Y += x**2*y
+    sumY2X += y**2*x
+    if image.is_off(row, col, offset=(0, -1)):
+      countVE += 1
+      sumYVE += y
+    if image.is_off(row, col, offset=(-1, 0)):
+      countHE += 1
+      sumXHE += x
+    if minRow is None or minRow > row:
+      minRow = row
+    if minCol is None or minCol > col:
+      minCol = col
+    if maxRow is None or maxRow < row:
+      maxRow = row
+    if maxCol is None or maxCol < col:
+      maxCol = col
+  features = [0] * 16
+  features[0] = minCol
+  features[1] = minRow
+  features[2] = maxCol - minCol
+  features[3] = maxRow - minRow
+  features[4] = count
+  features[5] = sumX / count
+  features[6] = sumY / count
+  features[7] = sumX2 / count
+  features[8] = sumY2 / count
+  features[9] = sumXY / count
+  features[10] = sumX2Y / count
+  features[11] = sumY2X / count
+  features[12] = countVE
+  features[13] = sumYVE
+  features[14] = countHE
+  features[15] = sumXHE
+  return features
 
 
 # diagonal base feature extraction
 def diagonal_features(image, **kwargs):
-	
-	zone_size = kwargs['zone_size']
-		
-	def zone_pixel_on(img, zone, row, col):
-		zone_row = zone // (img.width // zone_size) * zone_size + row
-		zone_col = zone % (img.width // zone_size) * zone_size + col
-		return img.is_on(zone_row, zone_col)
-		
-	def evaluate_zone(img, zone):
-		diagonals = [0.0] * (zone_size * 2 - 1)
-		for row in range(zone_size):
-			for col in range(zone_size):
-				diagonals[row + col] += zone_pixel_on(img, zone, row, col)
-		return stat.mean(diagonals)
-		
-	norm_img = image
-	num_zones = (norm_img.width // zone_size) * (norm_img.height // zone_size)
-	features = [0] * num_zones
-	for zone in range(num_zones):
-		features[zone] = evaluate_zone(norm_img, zone)
-	return features
+  
+  zone_size = kwargs['zone_size']
+    
+  def zone_pixel_on(img, zone, row, col):
+    zone_row = zone // (img.width // zone_size) * zone_size + row
+    zone_col = zone % (img.width // zone_size) * zone_size + col
+    return img.is_on(zone_row, zone_col)
+    
+  def evaluate_zone(img, zone):
+    diagonals = [0.0] * (zone_size * 2 - 1)
+    for row in range(zone_size):
+      for col in range(zone_size):
+        diagonals[row + col] += zone_pixel_on(img, zone, row, col)
+    return stat.mean(diagonals)
+    
+  norm_img = image
+  num_zones = (norm_img.width // zone_size) * (norm_img.height // zone_size)
+  features = [0] * num_zones
+  for zone in range(num_zones):
+    features[zone] = evaluate_zone(norm_img, zone)
+  return features
 
-	
-		
+  
+    
+    
+# hotspot feature extraction
+def hotspot_features(image):
+  num_directions = 8;
+  max_d = 3;
+  hotspots = []
+  for x in range(3,image.width,5):
+    for y in range(3,image.height,5):
+      hotspots.append((x,y))
+  def doDir(row,col,direction):
+    if direction == 0:
+      return row-1,col
+    elif direction == 1:
+      return row-1,col+1
+    elif direction == 2:
+      return row,col+1
+    elif direction == 3:
+      return row+1,col+1
+    elif direction == 4:
+      return row+1,col
+    elif direction == 5:
+      return row+1,col-1
+    elif direction == 6:
+      return row,col-1
+    elif direction == 7:
+      return row-1,col-1
+    else:
+      return row,col
+  def distanceToOn(img,row,col,direction,max_d):
+    for d in range(max_d):
+      row,col = doDir(row,col,direction)
+      if img.is_on(row,col):
+        return 85*d
+    return 255
+    
+  features = []
+  for row,col in hotspots:
+    features.append(row)
+    features.append(col)
+    for direction in range(num_directions):
+      features.append(direction)
+      features.append(distanceToOn(image,row,col,direction,max_d))
+  return features
--- generate_datasets.py
+++ generate_datasets.py
@@ -1,39 +1,52 @@
 from modules.DatasetGenerator import *
 
 def generate_stanford_diagonal(path='Stanford_data/diagonal.data', test=None):
-	generator = DatasetGenerator()
-	generator.load_Stanford()
-	generator.extract(fd.diagonal_features, parameters={'zone_size': 2}, normalize_to=256, test=test)	
-	generator.output(path, int_feature=True)
-	
+  generator = DatasetGenerator()
+  generator.load_Stanford()
+  generator.extract(fd.diagonal_features, parameters={'zone_size': 2}, normalize_to=256, test=test) 
+  generator.output(path, int_feature=True)
+  
 def generate_stanford_statistical(path='Stanford_data/statistical.data', test=None):
-	generator = DatasetGenerator()
-	generator.load_Stanford()
-	generator.extract(fd.statistical_features, normalize_to=256, test=test)	
-	generator.output(path, int_feature=False)
-	
+  generator = DatasetGenerator()
+  generator.load_Stanford()
+  generator.extract(fd.statistical_features, normalize_to=256, test=test) 
+  generator.output(path, int_feature=False)
+
+def generate_stanford_hotspot(path='Stanford_data/hotspot.data', test=None):
+  generator = DatasetGenerator()
+  generator.load_Stanford()
+  generator.extract(fd.hotspot_features, normalize_to=None, test=test) 
+  generator.output(path, int_feature=False)
+  
 def generate_stanford_raw(path='Stanford_data/raw.data', test=None):
-	generator = DatasetGenerator()
-	generator.load_Stanford()
-	generator.extract(None, test=test)
-	generator.output(path, int_feature=True)
+  generator = DatasetGenerator()
+  generator.load_Stanford()
+  generator.extract(None, test=test)
+  generator.output(path, int_feature=True)
 
 def generate_MNIST_diagonal(path='MNIST_data/diagonal.data', test=None):
-	generator = DatasetGenerator()
-	generator.load_MNIST()
-	generator.extract(fd.diagonal_features, parameters={'zone_size': 4}, normalize_to=256, test=test)	
-	generator.output(path, int_feature=True)	
-	
+  generator = DatasetGenerator()
+  generator.load_MNIST()
+  generator.extract(fd.diagonal_features, parameters={'zone_size': 4}, normalize_to=256, test=test) 
+  generator.output(path, int_feature=True)  
+  
 def generate_MNIST_statistical(path='MNIST_data/statistical.data', test=None):
-	generator = DatasetGenerator()
-	generator.load_MNIST()
-	generator.extract(fd.statistical_features, normalize_to=256, test=test)	
-	generator.output(path, int_feature=False)
+  generator = DatasetGenerator()
+  generator.load_MNIST()
+  generator.extract(fd.statistical_features, normalize_to=256, test=test) 
+  generator.output(path, int_feature=False)
+  
+def generate_MNIST_hotspot(path='MNIST_data/hotspot.data'):
+  extractor = DatasetGenerator()
+  extractor.load_MNIST()
+  extractor.extract(fd.hotspot_features, normalize_to=None, test=None)  
+  extractor.output(path, int_feature=True)
+ 
 
 def generate_MNIST_raw(path='MNIST_data/raw.data', test=None):
-	generator = DatasetGenerator()
-	generator.load_MNIST()
-	generator.extract(None, normalize_to=256, test=test)	
-	generator.output(path, int_feature=True)
-	
-generate_stanford_raw()
+  generator = DatasetGenerator()
+  generator.load_MNIST()
+  generator.extract(None, normalize_to=256, test=test)  
+  generator.output(path, int_feature=True)
+  
+generate_MNIST_hotspot()
