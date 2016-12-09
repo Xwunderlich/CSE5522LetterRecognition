f42d596fa0306d96eab3ed1091b3a624b6bfa07e
Chuhan Feng <simonrouse9461@gmail.com>
2016-12-09 18:30:35 -0500

    update

---
+++ modules/ModelTester.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from modules.DatasetLoader import *
+from modules.NNBuilder import *
+
+class ModelTester:
+	def __init__(self):
+		self.dataset = None
+		self.model = None
+		
+	@staticmethod
+	def format_time(time):
+		hour = int(time / 3600)
+		minute = int((time % 3600) / 60)
+		second = time % 60
+		return '{}:{:02}:{:05.2f}'.format(hour, minute, second)
+		
+	def prepare_data(self, dataset_path, class_list, fold, test_fold):
+		self.dataset = DatasetLoader(dataset_path, class_list)
+		self.dataset.load(one_hot=True)
+		self.dataset.fold(fold, test_fold)
+		
+	def build_NN_model(self, layers=[], activation=tf.nn.relu, dropout=None):
+		self.model = NNBuilder(self.dataset.attr_count, self.dataset.class_count)
+		for layer in layers:
+			self.model.add_layer(layer, activation=activation)
+		self.model.finish(dropout=dropout)
+		
+	def build_CNN_model(self, height, width, conv_layers=[], dense_layers=[], activation=tf.nn.relu, dropout=None):
+		self.model = CNNBuilder(height, width, dataset.class_count)
+		for conv in conv_layers:
+			self.model.add_conv_layer(conv, filter_size=(5, 5), activation=activation)	
+			self.model.add_pool_layer(2, 2)	
+		for dense in dense_layers:
+			self.model.add_layer(dense, activation=activation)	
+		self.model.finish(dropout=dropout)
+		
+	def train_model(self, algorithm, rate, iteration, batch_size=100, peek_interval=100, show_test=True):
+		self.model.train(self.dataset, algorithm=algorithm, rate=rate, iteration=iteration, batch_size=batch_size, peek_interval=peek_interval, show_test=show_test)
+		
+	def evaluate_model(self):
+		accuracy, loss = self.model.evaluate(self.dataset)
+		time = self.format_time(self.model.time)
+		print('RESULT: {:.2f}%   TIME: {}'.format(accuracy * 100, time))
--- testing.py
+++ testing.py
@@ -2,103 +2,126 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from modules.DatasetLoader import *
 from modules.NNBuilder import *
+from modules.ModelTester import *
 
-def format_time(time):
-	hour = int(time / 3600)
-	minute = int((time % 3600) / 60)
-	second = time % 60
-	return '{}:{:02}:{:05.2f}'.format(hour, minute, second)
 
 def test_MNIST_raw_softmax():
-	dataset = DatasetLoader('MNIST_data/raw', class_list=DatasetLoader.digits)
-	dataset.load(one_hot=True)
-	dataset.fold(10, test_fold=0)
+	tester = ModelTester()
+	tester.prepare_data('MNIST_data/raw', class_list=DatasetLoader.digits, fold=10, test_fold=0)
+	tester.build_NN_model([], activation=tf.nn.relu, dropout=None)											
+	tester.train_model(algorithm=tf.train.GradientDescentOptimizer, rate=2e-3, iteration=200000, batch_size=200, peek_interval=1000)
+	tester.evaluate_model()
 
-	neural_net = NNBuilder(dataset.attr_count, dataset.class_count)
-	neural_net.finish()												
-	neural_net.train(dataset, algorithm=tf.train.GradientDescentOptimizer, rate=2e-3, iteration=200000, batch_size=200, peek_interval=1000)
-	accuracy, loss = neural_net.evaluate(dataset)
-	time = format_time(neural_net.time)
-
-	print('RESULT: {:.2f}%   TIME: {}'.format(accuracy * 100, time))
+def test_MNIST_raw_1_layers():
+	tester = ModelTester()
+	tester.prepare_data('MNIST_data/raw', class_list=DatasetLoader.digits, fold=10, test_fold=0)
+	tester.build_NN_model([1000], activation=tf.nn.relu, dropout=None)											
+	tester.train_model(algorithm=tf.train.AdamOptimizer, rate=1e-3, iteration=100000, batch_size=100, peek_interval=500)
+	tester.evaluate_model()
+		
+def test_MNIST_raw_3_layers():
+	tester = ModelTester()
+	tester.prepare_data('MNIST_data/raw', class_list=DatasetLoader.digits, fold=10, test_fold=0)
+	tester.build_NN_model([500, 500, 500], activation=tf.nn.relu, dropout=0.5)											
+	tester.train_model(algorithm=tf.train.AdamOptimizer, rate=5e-5, iteration=20000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
 
 def test_MNIST_raw_CNN():
-	dataset = DatasetLoader('MNIST_data/raw', class_list=DatasetLoader.digits)
-	dataset.load(one_hot=True)
-	dataset.fold(10, test_fold=9)
-
-	neural_net = CNNBuilder(28, 28, dataset.class_count)						# create a convolutional neural net
-	neural_net.add_conv_layer(32, filter_size=(5, 5), activation=tf.nn.relu)	# add a colvolution layer with certain depth and filter size
-	neural_net.add_pool_layer(2, 2)												# add a 2x2 max pool layer
-	neural_net.add_conv_layer(64, filter_size=(5, 5), activation=tf.nn.relu)	# add a colvolution layer with certain depth and filter size
-	neural_net.add_pool_layer(2, 2)												# add a 2x2 max pool layer
-	neural_net.add_layer(1024, activation=tf.nn.relu)							# add a densely connected ReLU layer with certain amount of nodes
-	neural_net.finish(dropout=0.5)												# finish building the neural net with a dropout on the output layer
-	neural_net.train(dataset, algorithm=tf.train.AdamOptimizer, rate=2e-5, iteration=20000, batch_size=50, peek_interval=10, show_test=False)
-	accuracy, loss = neural_net.evaluate(dataset)
-	time = format_time(neural_net.time)
-
-	print('RESULT: {:.2f}%   TIME: {}'.format(accuracy * 100, time))
+	tester = ModelTester()
+	tester.prepare_data('MNIST_data/raw', class_list=DatasetLoader.digits, fold=10, test_fold=0)
+	tester.build_CNN_model(28, 28, conv_layers=[32, 64], dense_layers=[1024], activation=tf.nn.relu, dropout=0.5)
+	tester.train_model(algorithm=tf.train.AdamOptimizer, rate=1e-4, iteration=20000, batch_size=50, peek_interval=10, show_test=False)
+	tester.evaluate_model()
 
 def test_MNIST_statistical():
-	dataset = DatasetLoader('MNIST_data/statistical', class_list=DatasetLoader.digits)
-	dataset.load(one_hot=True)
-	dataset.fold(10, test_fold=9)
-
-	neural_net = NNBuilder(dataset.attr_count, dataset.class_count)		# create a neural net with the number of input and the number of output
-	neural_net.add_layer(500, activation=tf.nn.relu)						# add a ReLU layer with certain amount of nodes
-	neural_net.add_layer(500, activation=tf.nn.relu)						# add a ReLU layer with certain amount of nodes
-	neural_net.add_layer(500, activation=tf.nn.relu)						# add a ReLU layer with certain amount of nodes
-	neural_net.finish(dropout=0.5)											# create the output layer with a dropout
-	neural_net.train(dataset, algorithm=tf.train.AdamOptimizer, rate=1e-4, iteration=200000, batch_size=100, peek_interval=100)
-	accuracy, loss = neural_net.evaluate(dataset)
-	time = format_time(neural_net.time)
-	print('RESULT: {:.2f}%   TIME: {}'.format(accuracy * 100, time))
+	tester = ModelTester()
+	tester.prepare_data('MNIST_data/statistical', class_list=DatasetLoader.digits, fold=10, test_fold=0)
+	tester.build_NN_model([500, 500, 500], activation=tf.nn.relu, dropout=0.5)											
+	tester.train_model(algorithm=tf.train.AdamOptimizer, rate=1e-4, iteration=200000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
 	
 def test_MNIST_diagonal():
-	dataset = DatasetLoader('MNIST_data/diagonal', class_list=DatasetLoader.digits)
-	dataset.load(one_hot=True)
-	dataset.fold(10, test_fold=9)
+	tester = ModelTester()
+	tester.prepare_data('MNIST_data/diagonal', class_list=DatasetLoader.digits, fold=10, test_fold=0)
+	tester.build_NN_model([500, 500, 500], activation=tf.nn.relu, dropout=0.5)											
+	tester.train_model(algorithm=tf.train.AdamOptimizer, rate=1e-4, iteration=200000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
+	
+def test_MNIST_centroid():
+	tester = ModelTester()
+	tester.prepare_data('MNIST_data/centroid', class_list=DatasetLoader.digits, fold=10, test_fold=0)
+	tester.build_NN_model([500, 500, 500], activation=tf.nn.relu, dropout=0.5)											
+	tester.train_model(algorithm=tf.train.AdamOptimizer, rate=1e-4, iteration=200000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
+	
+def test_MNIST_hotspot():
+	tester = ModelTester()
+	tester.prepare_data('MNIST_data/hotspot', class_list=DatasetLoader.digits, fold=10, test_fold=0)
+	tester.build_NN_model([500, 500, 500], activation=tf.nn.relu, dropout=0.5)											
+	tester.train_model(algorithm=tf.train.AdamOptimizer, rate=1e-4, iteration=200000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
 
-	neural_net = NNBuilder(dataset.attr_count, dataset.class_count)		# create a neural net with the number of input and the number of output
-	neural_net.add_layer(500, activation=tf.nn.relu)						# add a ReLU layer with certain amount of nodes
-	neural_net.add_layer(500, activation=tf.nn.relu)						# add a ReLU layer with certain amount of nodes
-	neural_net.add_layer(500, activation=tf.nn.relu)						# add a ReLU layer with certain amount of nodes
-	neural_net.finish(dropout=0.5)											# create the output layer with a dropout
-	neural_net.train(dataset, algorithm=tf.train.AdamOptimizer, rate=1e-4, iteration=200000, batch_size=100, peek_interval=100)
-	accuracy, loss = neural_net.evaluate(dataset)
-	time = format_time(neural_net.time)
-	print('RESULT: {:.2f}%   TIME: {}'.format(accuracy * 100, time))
+def test_Irvine_statistical():
+	tester = ModelTester()
+	tester.prepare_data('Irvine_data/statistical', class_list=DatasetLoader.upper_letters, fold=10, test_fold=0)
+	tester.build_NN_model([500, 500, 500], activation=tf.nn.relu, dropout=0.5)											
+	tester.train_model(algorithm=tf.train.AdamOptimizer, rate=1e-4, iteration=40000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
 
 def test_stanford_raw_softmax():
-	dataset = DatasetLoader('Stanford_data/raw', class_list=DatasetLoader.lower_letters)
-	dataset.load(one_hot=True)
-	dataset.fold(10, test_fold=9)
-
-	neural_net = NNBuilder(dataset.attr_count, dataset.class_count)
-	neural_net.finish()												
-	neural_net.train(dataset, algorithm=tf.train.GradientDescentOptimizer, rate=1e-3, iteration=200000, batch_size=500, peek_interval=100)
-	accuracy, loss = neural_net.evaluate(dataset)
-	time = format_time(neural_net.time)
-
-	print('RESULT: {:.2f}%   TIME: {}'.format(accuracy * 100, time))
+	tester = ModelTester()
+	tester.prepare_data('Stanford_data/raw', class_list=DatasetLoader.lower_letters, fold=10, test_fold=0)
+	tester.build_NN_model([], activation=tf.nn.relu, dropout=None)											
+	tester.train_model(algorithm=tf.train.GradientDescentOptimizer, rate=5e-2, iteration=200000, batch_size=500, peek_interval=100)
+	tester.evaluate_model()
+	
+def test_stanford_raw_1_layer():
+	tester = ModelTester()
+	tester.prepare_data('Stanford_data/raw', class_list=DatasetLoader.lower_letters, fold=10, test_fold=0)
+	tester.build_NN_model([1000], activation=tf.nn.relu, dropout=None)											
+	tester.train_model(algorithm=tf.train.GradientDescentOptimizer, rate=1e-2, iteration=50000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
+	
+def test_stanford_raw_3_layer():
+	tester = ModelTester()
+	tester.prepare_data('Stanford_data/raw', class_list=DatasetLoader.lower_letters, fold=10, test_fold=0)
+	tester.build_NN_model([500, 500, 500], activation=tf.nn.relu, dropout=0.5)											
+	tester.train_model(algorithm=tf.train.GradientDescentOptimizer, rate=1e-4, iteration=50000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
 
 def test_stanford_raw_CNN():
-	dataset = DatasetLoader('Stanford_data/raw', class_list=DatasetLoader.lower_letters)
-	dataset.load(one_hot=True)
-	dataset.fold(10, test_fold=9)
-
-	neural_net = CNNBuilder(16, 8, dataset.class_count)						# create a convolutional neural net
-	neural_net.add_conv_layer(32, filter_size=(5, 5), activation=tf.nn.relu)	# add a colvolution layer with certain depth and filter size
-	neural_net.add_pool_layer(2, 2)												# add a 2x2 max pool layer
-	neural_net.add_conv_layer(64, filter_size=(5, 5), activation=tf.nn.relu)	# add a colvolution layer with certain depth and filter size
-	neural_net.add_pool_layer(2, 2)												# add a 2x2 max pool layer
-	neural_net.add_layer(1024, activation=tf.nn.relu)							# add a densely connected ReLU layer with certain amount of nodes
-	neural_net.finish(dropout=0.5)												# finish building the neural net with a dropout on the output layer
-	neural_net.train(dataset, algorithm=tf.train.AdamOptimizer, rate=2e-5, iteration=200000, batch_size=50, peek_interval=500)
-	accuracy, loss = neural_net.evaluate(dataset)
-	time = format_time(neural_net.time)
-
-	print('RESULT: {:.2f}%   TIME: {}'.format(accuracy * 100, time))
+	tester = ModelTester()
+	tester.prepare_data('Stanford_data/raw', class_list=DatasetLoader.lower_letters, fold=10, test_fold=0)
+	tester.build_CNN_model(28, 28, conv_layers=[32, 64], dense_layers=[1024], activation=tf.nn.relu, dropout=0.5)
+	tester.train_model(algorithm=tf.train.AdamOptimizer, rate=2e-5, iteration=20000, batch_size=50, peek_interval=10, show_test=False)
+	tester.evaluate_model()
 	
-test_stanford_raw_softmax()
+def test_Stanford_statistical():
+	tester = ModelTester()
+	tester.prepare_data('Stanford_data/statistical', class_list=DatasetLoader.lower_letters, fold=10, test_fold=0)
+	tester.build_NN_model([500, 500, 500], activation=tf.nn.relu, dropout=0.5)											
+	tester.train_model(algorithm=tf.train.GradientDescentOptimizer, rate=1e-4, iteration=80000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
+	
+def test_Stanford_diagonal():
+	tester = ModelTester()
+	tester.prepare_data('Stanford_data/diagonal', class_list=DatasetLoader.lower_letters, fold=10, test_fold=0)
+	tester.build_NN_model([500, 500, 500], activation=tf.nn.relu, dropout=0.5)											
+	tester.train_model(algorithm=tf.train.GradientDescentOptimizer, rate=1e-4, iteration=80000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
+	
+def test_Stanford_centroid():
+	tester = ModelTester()
+	tester.prepare_data('Stanford_data/centroid', class_list=DatasetLoader.lower_letters, fold=10, test_fold=0)
+	tester.build_NN_model([500, 500, 500], activation=tf.nn.relu, dropout=0.5)											
+	tester.train_model(algorithm=tf.train.GradientDescentOptimizer, rate=1e-4, iteration=80000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
+	
+def test_Stanford_hotspot():
+	tester = ModelTester()
+	tester.prepare_data('Stanford_data/hotspot', class_list=DatasetLoader.lower_letters, fold=10, test_fold=0)
+	tester.build_NN_model([500, 500, 500], activation=tf.nn.relu, dropout=0.5)											
+	tester.train_model(algorithm=tf.train.GradientDescentOptimizer, rate=1e-4, iteration=80000, batch_size=100, peek_interval=100)
+	tester.evaluate_model()
+	
+test_Irvine_statistical()
